7f74a8f8a80cbd76ef2f6c99b6133f5f2f56163c
DHP <qkqxk600@gmail.com>
2025-03-04 18:34:57 +0900

    lazy

--- Configs/zsh/.config/zsh/alias/alias_cd.zsh
+++ Configs/zsh/.config/zsh/alias/alias_cd.zsh
@@ -1,14 +1,10 @@
 #!/usr/bin/env zsh
-
-# Easier navigation: .., ..., ...., ....., ~ and -
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias -- -="cd -"
-
 alias cdd="cd $HOME/Downloads"
-
 function mkcd() {
     mkdir $1
     cd $1
