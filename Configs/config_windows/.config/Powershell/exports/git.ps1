e4a9fb78e2b4ba205d9e7b2d3bddfa31551ea44b
DHP <qkqxk600@gmail.com>
2025-03-19 16:46:51 +0900

    multimedia cli

--- Configs/config_windows/.config/Powershell/exports/git.ps1
+++ Configs/config_windows/.config/Powershell/exports/git.ps1
@@ -18,3 +18,24 @@ function lazyg {
     git commit -m "$args"
     git push
 }
+
+# multimedia 
+function yt ($url) {
+	yt-dlp --compat-options no-certifi -f "bv+ba/b" -S "ext"  $url
+}
+
+function Extract-Frames {
+    param (
+        [string]$inputFile,  # 입력 영상 파일
+        [string]$startTime,  # 시작 시간 (예: "00:01:30")
+        [int]$duration,      # 추출할 시간(초)
+        [string]$outputPrefix = "output"  # 출력 파일 접두사
+    )
+
+    # 출력 파일 이름 형식
+    $outputPattern = "${outputPrefix}_%04d.png"
+
+    # ffmpeg 명령 실행
+    $ffmpegCmd = "ffmpeg -i `"$inputFile`" -ss $startTime -t $duration -q:v 2 `"$outputPattern`""
+    Invoke-Expression $ffmpegCmd
+}
